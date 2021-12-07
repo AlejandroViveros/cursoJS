a3697ac9f16fb04a0eaa674a4342823b74faadeb
Alejandro Viveros <alejandroviverosm@gmail.com>
2021-12-07 15:39:02 -0300

    se termina la clase de arreglos N1

--- js/arreglos.js
+++ js/arreglos.js
@@ -29,3 +29,11 @@ arr.unshift("0");
 console.log(arr);
 // toString pone la representacion textual del arreglo, mas eficiente que Split pero no puede ingresar parametros
 console.log(arr.toString() );
+// a partir de la posicion se pueden borrar N cantidad de registros y tambien puede agregar, en este caso borrarmos el registro uno y agregamos en esa pos el numero 10
+arr.splice(1, 1, "10")
+console.log(arr);
+// corta de donde hasta donde le indiquemos e ignora lo demas
+arr = arr.slice(1, 3);
+console.log(arr);
+// length se usa para saber el largo del arreglo
+console.log(arr.length);
